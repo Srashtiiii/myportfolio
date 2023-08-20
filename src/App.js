b34f45dbea1b22b37bacea16a28c9070906aab75
srashtiiii <srashtig03@gmail.com>
2023-08-20 08:05:42 +0530

    day3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import Header from './Components/Header';
 import About from './Components/About';
+import Education from './Components/Education';
 import Projects from './Components/Projects';
 import Contact from './Components/Contact';
+
 
 function App() {
   return (
     <div className="App">
      <Header />
       <About />
+      <Education />
       <Projects />
       <Contact />
     
