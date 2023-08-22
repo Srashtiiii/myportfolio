bcb8115ad89dff4c5373feb892adb5f6049bd9f4
srashtiiii <srashtig03@gmail.com>
2023-08-22 08:55:08 +0530

    day3

--- src/Components/Education.js
+++ src/Components/Education.js
@@ -1,46 +1,16 @@
 import React from 'react'
 import './education.css';
+import { ReactComponent as WorkIcon} from '../assets/work.svg';
 
 const Education = () => {
-    const educationData = [
-        // Array of education objects, each containing details
-        {
-          title: 'Bachelor of Technology in Computer Science and Engineering',
-          institution:'GLA University, Mathura',
-          year: '2021 - 2024',
-        },
-        {
-          title: 'Diploma in Computer Science and Engineering',
-          institution: 'GLA University, Mathura',
-          year: '2017 - 2020',
-        },
-        {
-            title: 'High School',
-            institution: 'St. Dominics Sr. Sec. School.',
-            year: '2016 - 2017',
-          },
-        
-      ];
-    
+  
       return (
-        <section id="education" className="education-section">
-          <h2 className="education-heading">Education</h2>
-          <div className="timeline">
-        {educationData.map((education, index) => (
-          <div className={`timeline-item ${index % 2 === 0 ? 'left' : 'right'}`} key={index}>
-            <div className="timeline-dot"></div>
-            <div className="timeline-card">
-              <div className="timeline-content">
-                <h3>{education.title}</h3>
-                <p>{education.institution}</p>
-                <p>{education.year}</p>
-              </div>
-            </div>
-          </div>
-            ))}
-          </div>
-        </section>
+        <h1>education</h1>
+       
+      
   )
 }
 
 export default Education;
+
+
---
+++ src/Components/EducationTimeline.js
@@ -0,0 +1,29 @@
+let educationTimeLine = [
+    {
+      id: 1,
+      title: "Bachelor of Technology in Computer Science and Engineering",
+      location: "Mathura, Uttar Pradesh",
+      description: "GLA University",
+      date: "2021 - 2024",
+      icon: "school",
+    },
+    {
+      id: 2,
+      title: "Diploma in Computer Science and Engineering",
+      location: "Mathura, Uttar Pradesh",
+      description: "GLA University",
+      date: "2017 - 2020",
+      icon: "school",
+    },
+    {
+      id: 3,
+      title: "High School",
+      location: "Mathura , Uttar Pradesh",
+      description: "St. Dominics Sr. Sec. School.",
+      date: "2016 - 2017",
+      icon: "school",
+    },
+
+  ];
+  
+  export default educationTimeLine;
